6e6866ea1947cead6e344dac8c642e0463c14c0d
ludel <ludovic.delsol@ynov.com>
2018-04-04 17:48:50 +0200

    check language

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ import os
 
 def main():
     print("---- Train file ----")
-    train = get_all_files("train/")
+    train = get_files_and_language("train/")
 
     print("\n---- Test file ----")
-    test = get_all_files("test/")
+    test = get_files_and_language("test/", train.keys())
 
     train = load_data(train)
     test = load_data(test)
 
-    list_predict = list(prediction(train, test))
+    list_predict, score = prediction(train, test)
+    list_predict = list(list_predict)
 
-    print("\n ========= Prediction =========")
+    print(f"\n ========= Prediction score : {str(score*100)}% ========= ")
     for predict, real in zip(list_predict, list(test.keys())):
         check_prediction = "good prediction" if predict == real else "bad prediction"
-        print(f"- Language predict: {predict} / real: {real} ====> ", check_prediction)
+        print(f"- Language predict: {predict} / real: {real} ====> {check_prediction}")
 
     print("Process End")
 
@@ -31,8 +32,7 @@ def prediction(train, test):
     ])
 
     clf.fit(list(train.values()), list(train.keys()))
-
-    return clf.predict(test.values())
+    return clf.predict(test.values()), clf.score(list(test.values()), list(test.keys()))
 
 
 def load_data(files):
@@ -44,16 +44,19 @@ def load_data(files):
     return data
 
 
-def get_all_files(dir_path):
+def get_files_and_language(dir_path, possible_choice=""):
     files = {}
     for file in os.listdir(dir_path):
-        lang = prompt(f"What is the language of '{file}' file ? ({file})", file)
+        lang = prompt(f"What is the language of '{file}' file ? ({file}) ", file, possible_choice)
         files[lang] = dir_path + file
     return files
 
 
-def prompt(text, default):
+def prompt(text, default, possible_choice):
     inp = input(text)
+    while inp not in possible_choice and possible_choice:
+        print("Unknown language")
+        inp = input(text)
     if not inp:
         inp = default
     return inp
