155a4b964a67507d865668d8dd0f51163a061a7f
ludel <ludovic.delsol@ynov.com>
2018-04-19 00:19:54 +0200

    try it

---
+++ howToUse.py
@@ -0,0 +1,18 @@
+import os
+
+from sklearn.externals import joblib
+
+clf = joblib.load('modelTrain.pkl')
+
+# Defined your test directory
+DIR_TEST_PATH = "test/"
+
+data = []
+for file in os.listdir(DIR_TEST_PATH):
+    with open(DIR_TEST_PATH + file, 'r') as f:
+        data.append(f.read())
+        f.close()
+
+predict = clf.predict(data)
+
+print(predict)
